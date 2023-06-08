02f931c75d5aaaddcf70ba786c6603705feb8167
samruddhi1402 <samruddhi1422000@gmail.com>
2023-06-08 12:35:23 +0530

    project desciption edited

--- personal-portfollio-website/src/components/workCart/WorkData.jsx
+++ personal-portfollio-website/src/components/workCart/WorkData.jsx
@@ -6,7 +6,7 @@ const projectData = [
   {
     imgsrc: pro1,
     title: "Gym website",
-    text: "In this project we are creating a responsive gym website with Login and signup page where user can field all the details and then he can able to see all the respective pages also we are adding a pricing section where user can purchased his plain according to the price",
+    text: "In this project we are creating a responsive gym website with login and signup page where user can field all the details and then he can able to see all the respective pages also we are adding a pricing section where user can purchased his plain according to the price",
     view: "https://gym-website-d3dk.onrender.com",
     source: "https://github.com/samruddhi1402/Gym-Website-Project-Room-10.git",
   },
@@ -14,7 +14,7 @@ const projectData = [
   {
     imgsrc: pro2,
     title: "Twitter clone",
-    text: "In this project we are creating a twitter clone where user can add tweets in both feed and sidebar section also, also we add a validation of Login and sign up pages we are created 5 steps sign-up pages like a tweeter app and may other functionality like showmore and showless",
+    text: "In this project we are creating a twitter clone where user can add tweets in both feed and sidebar section also, also we add a validation of login and sign up pages we are created 5 steps sign-up pages like a tweeter app and many other functionality like showmore and showless",
     view: "https://celadon-phoenix-0ebb6b.netlify.app/",
     source: "https://github.com/samruddhi1402/twiiter_clone.git",
   },
@@ -22,7 +22,7 @@ const projectData = [
   {
     imgsrc: pro3,
     title: "Kanban Board",
-    text: "In this Kanban board project WE create a kanban board list where user can drag and drop the tasklist items in another list, w also add the dynamic routing where user can clicked any specific list item he can navigate to detaild page with unique id and add the description and activity for that perticular task we use react-dnd  ",
+    text: "In this Kanban board project we create a kanban board list where user can drag and drop the tasklist items in another list, we also add the dynamic routing where user can clicked any specific list item he can navigate to details page with unique id and add the description and activity for that perticular task we use react-dnd  ",
     view: "https://trello-2eou-1gbst1kw5-shubham-kumar-mca.vercel.app/",
     source: "https://github.com/samruddhi1402/Trello.git",
   },

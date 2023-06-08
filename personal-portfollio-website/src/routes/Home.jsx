8ef5b6823afbad73254ffc59d801ec4ca153662a
samruddhi1402 <samruddhi1422000@gmail.com>
2023-06-08 12:05:47 +0530

    home page edited

--- personal-portfollio-website/src/routes/Home.jsx
+++ personal-portfollio-website/src/routes/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import Heroimg from '../components/heroImages/Heroimg'
 import Footer from '../components/footer/Footer'
+import Work from '../components/workCart/Work'
 
 const Home = () => {
   return (
     <div>
      <Navbar/>
      <Heroimg/>
+     <Work/>
      <Footer/>
     </div>
   )
